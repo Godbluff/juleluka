86f2e5025ae7ade06260ce3081ee111b90dc0828
Bjørn Arve Lagim <bjornarvelagim@MacBookWIFI.lan>
2016-11-07 12:54:21 +0100

    Functioning 'luke'

--- src/Luke.js
+++ src/Luke.js
@@ -1,13 +1,42 @@
 import React, {Component} from 'react';
 import './styles/calendar-styles.css';
 
+let cardClass = 'card';
+let frontClass = 'face front';
+let backClass = 'face back';
+
 class Luke extends Component {
+
+    constructor(){
+        super();
+        this.openLuke = this.openLuke.bind(this);
+        this.state = {
+            isOpen: 0
+        }
+    }
+
+    openLuke(){
+        if(this.state.isOpen === 1){
+            cardClass = 'card';
+            this.setState({
+               isOpen: 0
+           });
+        }
+        if(this.state.isOpen === 0){
+            cardClass = 'card card-open';
+            this.setState({
+                isOpen: 1
+            })
+
+        }
+    }
+
     render(){
         return(
-            <div className="window-container">
-                <div className="card">
-                    <div className="face front">1</div>
-                    <div className="face back">Back</div>
+            <div className="window-container" onClick={this.openLuke}>
+                <div className={cardClass}>
+                    <div className={frontClass}>1</div>
+                    <div className={backClass}>'Wrap your presents well.'</div>
                 </div>
             </div>
         )
